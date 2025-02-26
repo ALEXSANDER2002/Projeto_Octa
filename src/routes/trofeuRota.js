b80042271b73354cb3726868844f3960dbd11621
kallebsous <kallebaraujo8@gmail.com>
2025-02-26 19:39:17 -0300

    sala-trofeu

--- src/routes/trofeuRota.js
+++ src/routes/trofeuRota.js
@@ -1,16 +1,14 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/sala-trofeu', (req, res) => {
+router.get('/trofeus', (req, res) => {
     const trofeus = [
-        { imagem: '/img/trofeusImg/trofeu1.jpg',
+        { imagem: '/img/trofeusImg/trofeu-master.jpg',
+            title: 'Campeã JIU 2023' },
+        { imagem: '/img/trofeusImg/trofeu-atletismo.jpg',
             title: 'Atletismo' },
-        { imagem: '/img/trofeusImg/trofeu2.jpg',
+        { imagem: '/img/trofeusImg/trofeu-handebol.jpg',
             title: 'Handebol' },
-        { imagem: '/img/trofeusImg/trofeu1.jpg',
-            title: 'Atletismo' },
-        { imagem: '/img/trofeusImg/trofeu2.jpg',
-            title: 'Handebol' }
     ];
 
     res.render('sala-trofeu', { trofeus }); // Enviando os dados para a view
