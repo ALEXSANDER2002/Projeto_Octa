cc4cc1908a4db3089dafa3231d520f538d60960a
ALEXSANDER2002 <alexsander18@unifesspa.edu.br>
2025-02-26 01:23:40 -0300

    integração com loja

--- src/routes/rotas.js
+++ src/routes/rotas.js
@@ -1,9 +1,15 @@
+const path = require('path');
 const express = require('express');
 const router = express.Router();
 
 // Rota para a Home
 router.get('/', (req, res) => {
     res.render('home'); // ✅ O Express buscará "home.ejs" dentro de "src/views/"
+});
+
+// Rota para a loja
+router.get('/loja', (req, res) => {
+    res.render('loja'); // ✅ O Express buscará "loja.ejs" dentro de "src/views/"
 });
 
 module.exports = router;
